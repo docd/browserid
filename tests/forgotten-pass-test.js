4d063a5bde58292be112f315d0397293749ee004
Zachary Carter <zack.carter@gmail.com>
2012-07-10 17:53:30 -0700

    add validation for email_for_token and user_creation_status

--- tests/forgotten-pass-test.js
+++ tests/forgotten-pass-test.js
@@ -181,6 +181,8 @@ suite.addBatch({
     },
     "account created": function(err, r) {
       assert.equal(r.code, 200);
+      console.log('body', r.body);
+      console.log(r);
       var body = JSON.parse(r.body);
       assert.strictEqual(body.success, true);
     }
--- tests/stalled-mysql-test.js
+++ tests/stalled-mysql-test.js
@@ -138,7 +138,7 @@ suite.addBatch({
   },
   "email_for_token": {
     topic: wsapi.get('/wsapi/email_for_token', {
-      token: 'bogus'
+      token: 'bogusbogusbogusbogusbogusbogusbogusbogusbogusbog'
     }),
     "fails with 503": function(err, r) {
       assert.strictEqual(r.code, 503);
@@ -229,7 +229,7 @@ suite.addBatch({
   "cert_key": {
     topic: wsapi.post('/wsapi/cert_key', {
       email: "test@whatev.er",
-      pubkey: "bogus",
+      pubkey: JSON.stringify("bogusbogusbogusbogusbogusbogusbogusbogusbogusbogusbogus"),
       ephemeral: false
     }),
     "fails with 503": function(err, r) {
